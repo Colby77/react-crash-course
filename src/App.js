4afb227325acb87af53392af4d94f396d132cb02
Colby Workman <cgw2319@gmail.com>
2022-03-01 11:21:23 -0700

    can toggle reminder value in db.json

--- src/App.js
+++ src/App.js
@@ -28,6 +28,15 @@ const fetchTasks = async () => {
   return data
   }
 
+// Fetch one task
+const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+  
+    console.log(data)
+    return data
+}
+
 // Add Task
 const addTask = async (task) => {
 
@@ -62,8 +71,21 @@ const deleteTask = async (id) => {
 // Toggle Reminder
 const toggleReminder = async (id) => {
 
+  const taskToToggle = await fetchTask(id)
+  const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updatedTask)
+  })
+
+  const data = await res.json()
+
   setTasks(tasks.map((task) => task.id === id
-   ? {...task, reminder: !task.reminder} : task))
+   ? {...task, reminder: data.reminder} : task))
 }
 
   return (
